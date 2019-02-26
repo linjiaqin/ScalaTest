bb868b7fc0f7b46c2ed42b7b105d2d81866cd5cf
linjiaqin <1263457581@qq.com>
2019-02-26 20:52:37 +0800

    add json read test by ljq

--- src/main/scala/com/ljq/JsonTest.scala
+++ src/main/scala/com/ljq/JsonTest.scala
@@ -1,5 +1,28 @@
 package com.ljq
 
-object JsonTest {
+import org.apache.spark.{SparkConf, SparkContext}
+import org.json4s.DefaultFormats
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 
+case class Person(name: String, age: Int)
+
+object JsonTest {
+  def main(args:Array[String])={
+    var conf = new SparkConf().setAppName("JsonTest").setMaster("local")
+    var jarPath = "/home/linjiaqin/IdeaProjects/ScalaTest/out/artifacts/ScalaTest_jar/ScalaTest.jar"
+
+    var sc = new SparkContext(conf)
+    sc.addJar(jarPath)
+
+    var hdfs = "hdfs://localhost:9000"
+    var input = hdfs + "/linjiaqin/3_data/testjson.json"
+
+    var jsontext = sc.textFile(input)
+
+    implicit val formats = DefaultFormats
+
+    var person = jsontext.collect().map(x => parse(x).extract[Person] )
+    person.foreach(println)
+  }
 }
